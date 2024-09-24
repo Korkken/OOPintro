6cf87db298866e76e5d2eb2449609b9e4780d768
Tim Lappalainen <timnhl95@hotmail.com>
2024-09-24 14:20:05 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Console.WriteLine("Enter the radius of the circle:");
+int radius = int.Parse(Console.ReadLine());
+Circle circle = new Circle(radius);
+double area = Math.Round(circle.GetArea(), 2);
+Console.WriteLine($"The area of the circle with radius {radius} is: {area}");
+
+class Circle
+{
+    public int Radius { get; set; }
+    public Circle(int radius)
+    {
+        Radius = radius;
+    }
+    public double GetArea()
+    {
+        return Math.PI * Math.Pow(Radius, 2);
+    }
+
+}
+

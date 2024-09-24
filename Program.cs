f2c1308cc9cf40e87d3cc3a2e906f61bf0a9caf7
Tim Lappalainen <timnhl95@hotmail.com>
2024-09-24 14:38:02 +0200

    added loop to check if the user put a positive integer or not.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,11 @@
 
 
 Console.WriteLine("Enter the radius of the circle:");
-int radius = int.Parse(Console.ReadLine());
+int radius;
+while (!int.TryParse(Console.ReadLine(), out radius) || radius <= 0)
+{
+    Console.WriteLine("Skriv in en siffra eller ett tal som är större än 0");
+}
 Circle circle = new Circle(radius);
 double area = Math.Round(circle.GetArea(), 2);
 Console.WriteLine($"The area of the circle with radius {radius} is: {area}");
